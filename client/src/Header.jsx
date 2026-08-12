810cb767acc0fc42c2df5182afe2eac2985687e4
IRFAN <mmirfan2232@gmail.com>
2026-08-12 21:52:44 +0500

    changes

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -1,7 +1,14 @@
 import React , {useState} from "react";
 function Header(){
     return(
-        <div>this is header</div>
+        
+              <div className="header">
+                <h2>sellora</h2>
+                 <img src="src/assets/plp-solo4-cloudpink.jpg"  alt="https://placehold.co/150"  alt="profile pic" />
+            <p>this is name</p>
+            <input type="button" value="edit" onClick={()=>setIsEditing(true)} />
+        </div>
+        
     )
 };
 export default Header;
--- client/src/Profile.jsx
+++ client/src/Profile.jsx
@@ -1,49 +1,9 @@
 import React , {useState} from "react";
 function Profile(props){
-    const [isEditing , setIsEditing] = useState(false)
-    const [profile , setProfile] = useState([
-        profileName = props.name,
-         profileImg = props.img
-
-    ])
-    // function editProfile(){
-    //     setIsEditing = true
-    // }
-
-    function handleChange(e){
-        const {name , value}= e.target
-        setProfile(prev , {
-            ...prev
-            [name] = value
-        })
-
-    }
+ 
 
     return(
-        <div>
-            {isEditing?(
-                  <form action="">
-            <label htmlFor="img">select image</label>
-            <img src={imgSrc} alt="select pic"  onChange={handleChange} value={profile.img} aria-required/>
-            <label htmlFor="name">choose name</label>
-            <input type="text" onChange={handleChange} value={profile.name} />
-            <button type="submit" onClick={handleClick}>save</button>
-            </form>
-            ):(
-                <>
-                 <img src="src/assets/hero.png" alt="profile pic" />
-            <p>this is name</p>
-          
-            <input type="button" value="edit" onClick={()=>setIsEditing(true)} />
-            </>
-            )}
-            
-          
-            
-           
-            
-           
-        </div>
+      <div>profile</div>
     )
 };
 export default Profile;
