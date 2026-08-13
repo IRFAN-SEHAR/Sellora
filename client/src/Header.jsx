0a41878e43efb8fe2409b73b6625b687905bd62c
IRFAN <mmirfan2232@gmail.com>
2026-08-13 23:51:57 +0500

    changes

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -4,6 +4,13 @@ function Header(){
         
               <div className="header">
                 <h2>sellora</h2>
+                <p>home</p>
+                <p>category</p>
+                <div className="searchBtn">
+                <label htmlFor="search">search</label>
+                <input  type="text" aria-required/>
+                <input type="button" value="button" />
+                </div>
                  <img src="src/assets/plp-solo4-cloudpink.jpg"  alt="https://placehold.co/150"  alt="profile pic" />
             <p>this is name</p>
             <input type="button" value="edit" onClick={()=>setIsEditing(true)} />
--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -2,11 +2,7 @@ import React , {useState} from "react";
 function Home(){
     return(
         <div className="home">
-            <div className="searchBtn">
-                <label htmlFor="search">search</label>
-                <input  type="text" aria-required/>
-                <input type="button" value="button" />
-                </div>
+            
                 
         </div>
     )
