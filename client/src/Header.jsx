7ccca4425f56e4a34c0ae12de037a1b19557ff55
IRFAN <mmirfan2232@gmail.com>
2026-08-14 22:04:32 +0500

    header

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -6,15 +6,25 @@ function Header(){
                 <h2>sellora</h2>
                 <p>home</p>
                 <p>category</p>
-                <div className="searchBtn">
                 <label htmlFor="search">search</label>
                 <input  type="text" aria-required/>
-                <input type="button" value="button" />
+                <svg  className="search-icon" width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path
+    d="M21.2 18.9C22.2 17.5 22.8 15.8 22.8 14C22.8 9.1 18.9 5.2 14 5.2C9.1 5.2 5.2 9.1 5.2 14C5.2 18.9 9.1 22.8 14 22.8C15.8 22.8 17.5 22.2 18.9 21.2"
+    stroke="currentColor"
+    stroke-width="2.4"
+    stroke-linecap="round"
+  />
+  <path
+    d="M19 19L26.8 26.8"
+    stroke="currentColor"
+    stroke-width="2.4"
+    stroke-linecap="round"
+  />
+  <circle cx="14" cy="14" r="1.6" fill="currentColor"/>
+</svg>
                 </div>
-                 <img src="src/assets/plp-solo4-cloudpink.jpg"  alt="https://placehold.co/150"  alt="profile pic" />
-            <p>this is name</p>
-            <input type="button" value="edit" onClick={()=>setIsEditing(true)} />
-        </div>
+    
         
     )
 };
