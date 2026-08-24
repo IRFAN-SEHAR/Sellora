afb4687b08e3c3aae514c3cc0405686c27922aa0
IRFAN <mmirfan2232@gmail.com>
2026-08-24 23:07:55 +0500

    new changes

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -6,24 +6,22 @@ const[menuOpen , setMenuOpen] = useState(false)
     return(
         
               <div className="header">
-                <div className="nav-items">
-                <logo>VELYN</logo>
-                <ul  className={menuOpen ? "links-open" : "links" } >
-              <li> <a href="#">home</a></li> 
-              <li>  <a href="#">category</a>            </li>   
+                <h1 className="logo" >VELYN</h1>
+                <ul className="nav-list"  className={menuOpen ? "links-open" : "links" } >
+              <li><a href="#">home</a></li> 
+              <li> <a href="#">category</a> </li>   
                    <li className="searchBtn">
                  <label htmlFor="search">
                   <span class="material-symbols-outlined">search</span>
                   </label>
                     <input  type="text" aria-required/>
-                </li>
-              <li className="login">     
-<button className="loginBtn">login</button>
-<button className="signupBtn">signup</button>
-</li>
+                </li>             
+<li><button className="loginBtn">login</button></li>
+<li><button className="signupBtn">signup</button></li>
+
 </ul>
 <button id="menu" onClick={()=>setMenuOpen(!menuOpen)}>☰</button>
-</div>
+
                 </div>
     )
 };
