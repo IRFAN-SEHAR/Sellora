9e3adc2e02b3fa54378a6f6d0a6f966cb979ec46
IRFAN <mmirfan2232@gmail.com>
2026-08-25 22:02:35 +0500

    new

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -7,15 +7,13 @@ const[menuOpen , setMenuOpen] = useState(false)
         
               <div className="header">
                 <h1 className="logo" >VELYN</h1>
-                <ul className="nav-list"  className={menuOpen ? "links-open" : "links" } >
+                <ul  className={menuOpen ? "links-open" : "links" } >
               <li><a href="#">home</a></li> 
               <li> <a href="#">category</a> </li>   
-                   <li className="searchBtn">
-                 <label htmlFor="search">
-                  <span class="material-symbols-outlined">search</span>
-                  </label>
+                   <li className="search-input">
                     <input  type="text" aria-required/>
-                </li>             
+                </li>    
+                <li>  <span className="material-symbols-outlined">search</span></li>    
 <li><button className="loginBtn">login</button></li>
 <li><button className="signupBtn">signup</button></li>
 
